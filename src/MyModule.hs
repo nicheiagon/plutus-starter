a4577869b836140b0640ed3b7841fd446a94fdff
Niko Cherkezishvili <nikolai.cherkezishvili@iagon.com>
2022-05-10 16:10:18 +1000

    bug reproduced

--- src/MyModule.hs
+++ src/MyModule.hs
@@ -1,7 +1,97 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module MyModule where
 
-hello :: String
-hello = "hello"
+import           Control.Monad        hiding (fmap)
+import           Data.Map             as Map
+import           Data.Text            (Text)
+import           Data.Void            (Void)
+import           Plutus.Contract
+import           PlutusTx             (Data (..))
+import qualified PlutusTx
+import           PlutusTx.Prelude     hiding (Semigroup(..), unless)
+import           Ledger               hiding (singleton)
+import           Ledger.Constraints   as Constraints
+import qualified Ledger.Typed.Scripts as Scripts
+import           Ledger.Ada           as Ada
+import           Playground.Contract  (printJson, printSchemas, ensureKnownCurrencies, stage)
+import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types     (KnownCurrency (..))
+import           Prelude              (IO, Semigroup (..), String)
+import           Text.Printf          (printf)
 
-main :: IO ()
-main = putStrLn hello
+
+newtype MySillyRedeemer = MySillyRedeemer Integer
+
+PlutusTx.unstableMakeIsData ''MySillyRedeemer
+
+{-# INLINABLE mkValidator #-}
+mkValidator :: () -> MySillyRedeemer -> ScriptContext -> Bool
+mkValidator _ (MySillyRedeemer r) _ = traceIfFalse "BOOM FAIL" False
+
+data Typed
+instance Scripts.ValidatorTypes Typed where
+    type instance DatumType Typed = ()
+    type instance RedeemerType Typed = MySillyRedeemer
+
+typedValidator :: Scripts.TypedValidator Typed
+typedValidator = Scripts.mkTypedValidator @Typed
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @() @MySillyRedeemer
+
+validator :: Validator
+validator = Scripts.validatorScript typedValidator
+
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash typedValidator
+
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
+
+create :: AsContractError e => () -> Contract w s e ()
+create () = do
+    let tx = mustPayToTheScript () $ Ada.lovelaceValueOf 10000000
+    ledgerTx <- submitTxConstraints typedValidator tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    logInfo @String "Created BOOM"
+
+updateFile :: forall w s e. AsContractError e => () -> Contract w s e ()
+updateFile () = do
+    logInfo @String "UPDATE CALLED"
+    utxos <- utxosAt scrAddress
+    let orefs   = fst <$> Map.toList utxos
+        lookups = Constraints.unspentOutputs utxos      <>
+                  Constraints.otherScript validator
+        tx :: TxConstraints Void Void
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toBuiltinData (MySillyRedeemer 10000000) | oref <- orefs]
+    logInfo @String $ "COMMITTING TRANSACTION"
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    logInfo @String $ "SHOULD NOT GET HERE"
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    logInfo @String $ "DEFINITELY SHOULD NOT GET HERE"
+
+type StorageSchema =
+  Endpoint "create" () .\/ Endpoint "updateFile" ()
+
+endpoints :: Contract () StorageSchema ContractError ()
+endpoints = selectList [create', updateFile'] >> endpoints
+  where
+    create' = endpoint @"create" create
+    updateFile' = endpoint @"updateFile" updateFile
---
+++ test/Spec/Trace.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Spec.Trace where
+
+import Data.Functor
+import Data.Monoid
+import Data.Text
+import Ledger (ScriptError (EvaluationError), ValidationError (ScriptFailure))
+import qualified Ledger.Ada as Ada
+import Plutus.Contract
+import Plutus.Contract.Test
+import qualified Plutus.Trace.Emulator as Trace
+import Plutus.Trace.Emulator.Types (ContractInstanceTag)
+import Test.Tasty
+import Wallet.API (WalletAPIError (ValidationError))
+import Ledger.Index (ScriptValidationEvent)
+import Ledger (Tx)
+import Plutus.Trace.Emulator (EmulatorTrace, getContractState)
+import qualified Plutus.Trace.Emulator as Emulator
+import MyModule
+
+
+t1, t2 :: ContractInstanceTag
+t1 = Trace.walletInstanceTag w1
+t2 = Trace.walletInstanceTag w2
+
+tests :: TestTree
+tests =
+  testGroup
+    "Basic storage use-cases"
+    [ checkPredicate
+        "Should fail to update file has with same name"
+        ( assertNoFailedTransactions
+          -- assertContractError endpoints t1 (scriptErrorPredicate "fail") "should fail during validation process"
+            -- .&&. assertAccumState theContract t1 ((Last $ Just $ CreatedFile "some-file-hash-1") ==) "Final state for w1 should be CreatedFile"
+        )
+        $ do
+          hd1 <- Trace.activateContractWallet w1 endpoints
+          Trace.callEndpoint @"create" hd1 ()
+          void $ Trace.waitNSlots 1
+          Trace.callEndpoint @"updateFile" hd1 ()
+          void $ Trace.waitNSlots 1
+--          void $ getContractState hd1
+    ]
+
+scriptErrorPredicate :: Text -> ContractError -> Bool
+scriptErrorPredicate errorMsg (WalletContractError (ValidationError (ScriptFailure (EvaluationError allErrors _)))) = errorMsg `elem` allErrors
+scriptErrorPredicate errorMsg (OtherContractError err) = err == errorMsg
+scriptErrorPredicate _ _ = False
+
+--transactionFailedPredicate :: Text -> Tx -> ValidationError -> [ScriptValidationEvent] -> Bool
+--transactionFailedPredicate errorMsg _ (ScriptFailure (EvaluationError allErrors _)) _ = errorMsg `elem` allErrors
+--transactionFailedPredicate _ _ _ _ = False
